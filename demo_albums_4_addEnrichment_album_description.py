b505cc0d015ce5cde6628df3204d65859007c15b
burche1 <burche.t.d@gmail.com>
2020-05-31 14:44:06 -0300

    Add files via upload

---
+++ demo_albums_4_addEnrichment_album_description.py
@@ -0,0 +1,48 @@
+from init_photo_service import service
+import pandas as pd
+
+response = service.albums().list(
+    pageSize=50,
+    excludeNonAppCreatedData=False
+).execute()
+# get's albums information's : id, title, productURL, mediaItensCount, coverPhotoBaseUrl, coverPhotoMediaItemId
+ 
+lstAlbums = response.get('albums')              # same result as response, cause there is no nextPageToken available
+nextPageToken = response.get('nextPageToken')   # result is none to my library, cause I have less albuns than the pageSize limit
+
+while nextPageToken:
+    response = service.albums().list(
+        pageSize=50,
+        excludeNonAppCreatedData=False,
+        pageToken=nextPageToken
+    )
+    lstAlbums.append(response.get('albums'))
+    nextPageToken = response.get('nextPageToken')
+
+# using pandas to better configure the lstAlbums created
+df_albums = pd.DataFrame(lstAlbums)
+
+my_album_id = df_albums[df_albums['title']=='My Family Photos']['id'][3]
+
+# using this my_album_id, get information's about this album
+response = service.albums().get(albumId=my_album_id).execute()
+
+"""
+addEnrichment (album description)
+"""
+
+request_body = {
+    'newEnrichmentItem': {
+        'textEnrichment': {
+            'text': 'This is my family album'
+        }
+    },
+    'albumPosition': {
+        'position': 'LAST_IN_ALBUM'
+    }
+}
+ 
+response = service.albums().addEnrichment(
+    albumId=my_album_id,
+    body=request_body
+).execute()
